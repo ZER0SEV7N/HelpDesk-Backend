50c031253a1187ad6a6790de24e766d407fc735f
Mikepro149 <miguelito89popo@gmail.com>
2026-01-22 16:09:47 -0500

    Third commit

---
+++ src/entities/empresa.entity.ts
@@ -0,0 +1,37 @@
+//Modulo de entidad para la tabla empresa
+//importaciones necesarias:
+import { Entity, Column, PrimaryGeneratedColumn, JoinColumn, ManyToOne } from 'typeorm';
+
+//Definicion de la entidad Empresa
+@Entity('Empresa')
+export class Empresa {
+    //Columna para el ID de la empresa
+    @PrimaryGeneratedColumn()
+    id_empresa: number; //Llave primaria auto-generada
+
+    //Columna para el nombre de la empresa
+    @Column()
+    nombre_cliente: string; //Nombre de la empresa
+
+    //Columna para el RUC de la empresa (Maximo 11 caracteres)
+    @Column( { length: 11 } )
+    ruc: string; //RUC de la empresa
+
+    //Columna para la direccion
+    @Column()
+    direccion: string; //Direccion de la empresa
+
+    //Columna para el telefono
+    @Column()
+    telefono: string; //Telefono de la Empresa
+
+    //Columna para el correo
+    @Column()
+    correo: string; //Correo de la empresa
+
+    //Relacion con la tabla Sucursales (Un empresa puede tener muchas sucursales)
+    //ManyToOne
+    @ManyToOne( () => Empresa, empresa => empresa.id_empresa )
+    @JoinColumn( { name: 'id_empresa' } )
+    empresa: Empresa;
+}
