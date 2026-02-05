b03e8460b6645774c89acfe85b2c744a0893d7a1
alexander <malaverroshan2@gmail.com>
2026-02-05 15:44:26 -0500

    VivaSenati

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { 
+    Body, 
+    Controller,
+    Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import {LoginDTO } from './dto/login.auth.dto';
+import { RegisterDTO } from './dto/register.auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+  // REGISTER
+@Post('register')
+register(@Body() dto: RegisterDTO) {
+    return this.authService.register(dto);
+}
+
+  // LOGIN
+@Post('login')
+login(@Body() dto: LoginDTO) {
+    return this.authService.login(dto);
+}
+}
